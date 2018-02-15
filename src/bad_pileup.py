f5469c7af8ae3bd11a67ecc2ea5d15eee7e6ffd9
meganm22 <megan.mcghie@icloud.com>
2018-02-14 16:53:01 -0800

    Update bad_pileup.py

--- src/bad_pileup.py
+++ src/bad_pileup.py
@@ -5,28 +5,31 @@
 
 def pileup(reads, outfile):
     snps = {}
-    ins = {}
     dels = {}
+    ins = {}
 
     for line in reads:
         line = line.split('\t')
         pos_aligned = line[3]
-        if (line[4] != '.'):
-            read_snps = line[4].split(',')
-            for val in read_snps:
-                actual_pos = pos_aligned + val
+        if (line[4] != '.'): ## SNP
+            read_snps_pos = line[4].split(',')
+            read_snps_ref = line[5].split(',')
+            for i in range(len(read_snps_pos)):
+                read_snp_p = int(read_snps_pos[i])
+                read_snp_r = read_snps_ref[i]
+                actual_pos = pos_aligned + read_snp_p
                 if ((str(actual_pos),line[0][val]) in snps.keys()):
                     snps[(str(actual_pos),line[0][val])] = snps[(str(actual_pos),line[0][val])] + 1
                 else:
                     snps[(str(actual_pos),line[0][val])] = 1
-        if (line[5] != '.'):
-            read_ins = line[5].split(',')
-            for val in read_ins:
+        if (line[6] != '.'): ## DEL
+            read_del = line[6].split(',')
+            for val in read_del:
                 actual_pos = pos_aligned + val
-                if ((str(actual_pos),line[0][val:val+line[6]]) in ins.keys()):
-                    ins[(str(actual_pos),line[0][val:val+line[6]])] = ins[(str(actual_pos),line[0][val:val+line[6]])] + 1
+                if ((str(actual_pos),line[0][val:val+line[6]]) in dels.keys()):
+                    dels[(str(actual_pos),line[0][val:val+line[6]])] = dels[(str(actual_pos),line[0][val:val+line[6]])] + 1
                 else:
-                    ins[(str(actual_pos),line[0][val:val+line[6]])] = 1
+                    dels[(str(actual_pos),line[0][val:val+line[6]])] = 1
     outfile.write(">SNP\n")
     for key in snps:
         if snps[key] > 5:
